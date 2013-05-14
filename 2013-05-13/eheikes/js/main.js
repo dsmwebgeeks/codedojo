1faee485acdd9df52e74f0cb6030cb61ea172003
Eric Heikes <eheikes@gmail.com>
2013-05-13 19:43:36 -0500

    Basic Ajax call.

---
+++ 2013-05-13/eheikes/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  var path = '/codedojo/2013-05-13/eheikes/';
+
+  $.getJSON(path + 'petitions/list.js')
+  .done(function(data, textStatus, jqXHR) {
+    console.log(data);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(jqXHR);
+    console.log(textStatus);
+    console.log(errorThrown);
+  });
+});
